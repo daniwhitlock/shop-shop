62a4ab0d29f5b3888a9630679436753cd4e03425
Fire Dragon <daniwhitlock4@gmail.com>
2021-04-18 21:21:08 -0600

    finished adding payment

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from "react";
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from "../../utils/helpers";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
@@ -6,15 +9,26 @@ import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
 
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session })
+      })
+    }
+  }, [data]);
 
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
       dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
     };
-  
+
     if (!state.cart.length) {
       getCart();
     }
@@ -30,6 +44,20 @@ const Cart = () => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+   
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
   }
 
   if (!state.cartOpen) {
@@ -57,22 +85,22 @@ const Cart = () => {
 
             {
               Auth.loggedIn() ?
-                <button>
+                <button onClick={submitCheckout}>
                   Checkout
-              </button>
+                </button>
                 :
                 <span>(log in to check out)</span>
             }
           </div>
         </div>
       ) : (
-          <h3>
-            <span role="img" aria-label="shocked">
-              😱
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
           </span>
           You haven't added anything to your cart yet!
-          </h3>
-        )}
+        </h3>
+      )}
     </div>
   );
 };
